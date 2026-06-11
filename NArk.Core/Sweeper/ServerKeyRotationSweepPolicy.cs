113fa59ad22a5c1e403529e78e451ac67ccea12c
d4rp4t <d4rp4t@tutanota.com>
2026-06-11 22:04:35 +0200

    fix: cutoffDate is represented in seconds

--- NArk.Core/Sweeper/ServerKeyRotationSweepPolicy.cs
+++ NArk.Core/Sweeper/ServerKeyRotationSweepPolicy.cs
@@ -8,12 +8,12 @@ public class ServerKeyRotationSweepPolicy(IClientTransport clientTransport): ISw
 {
     public async IAsyncEnumerable<ArkCoin> SweepAsync(IEnumerable<ArkCoin> coins, CancellationToken cancellationToken = default)
     {
-        var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         var serverInfo = await clientTransport.GetServerInfoAsync(cancellationToken);
         
         var recoverableKeys = serverInfo.DeprecatedSigners
             // TODO(11.06.2026) remove this nullable cast after protobuf update, or just remove null check if unused
-            .Where(ds=>ds.Value > now || (long?)ds.Value is null)  
+            .Where(ds=> ds.Value > now || (long?)ds.Value is null)  
             .Select(ds => ds.Key)
             .ToArray();
 
